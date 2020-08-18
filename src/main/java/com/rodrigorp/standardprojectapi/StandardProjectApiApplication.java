ef7f62fdce90c9de55b42f51f12773c0b20f44a1
RodrigoRP <grohlbr@hotmail.com>
2020-08-18 18:51:13 -0300

    first commit

---
+++ src/main/java/com/rodrigorp/standardprojectapi/StandardProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigorp.standardprojectapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandardProjectApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StandardProjectApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rodrigorp/standardprojectapi/StandardProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigorp.standardprojectapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StandardProjectApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
